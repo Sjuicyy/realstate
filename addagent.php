213fbdd02bbde9a9efc3c76f07594c9fedf51be6
Shyam Joshi <sjuicyy@gmail.com>
2023-01-17 17:01:21 +0545

    update

--- addagent.php
+++ addagent.php
@@ -1,21 +1,21 @@
 <?php
 if (isset($_POST['submit'])) {
-    $name= ucwords($_POST['name']);
-    $address= ucwords($_POST['address']);
+    $name = ucwords($_POST['name']);
+    $address = ucwords($_POST['address']);
     $phone = $_POST['phone'];
     $email = $_POST['email'];
     $pswd = $_POST['password'];
     $about = ucfirst($_POST['about']);
-    $date=date("Y-m-d");
-    $image= $_FILES['photo']['name'];
+    $date = date("Y-m-d");
+    $image = $_FILES['photo']['name'];
     $target2 = "img/Agent/" . basename($image);
     require_once 'Connection.php';
     $sql = "INSERT INTO `agent` (`id`, `name`, `email`, `phone`, `address`, `password`, `image`, `about`, `area`, `facebook`, `otp`, `date`, `status`) 
                             VALUES (NULL, '$name', '$email', '$phone', '$address', '$pswd', '$image', '$about', NULL, NULL, NULL, '$date', NULL)";
-  mysqli_query($conn,$sql);
-    if (move_uploaded_file($_FILES['photo']['tmp_name'], $target2))  {
+    mysqli_query($conn, $sql);
+    if (move_uploaded_file($_FILES['photo']['tmp_name'], $target2)) {
         echo "<script>alert('Successfully Submitted !');</script>";
-          header('Location: index.php');
+        header('Location: index.php');
     } else {
         echo "<script>alert('Something went wrong! Please Retry');</script>";
     }
--- addproperty.php
+++ addproperty.php
@@ -1,34 +1,30 @@
-
-
 <?php
 if (isset($_POST['submit'])) {
     require_once 'Connection.php';
     session_start();
-    $id=$_SESSION['id'];
-    $name=ucwords($_POST['name']);
-    $address=ucwords($_POST['address']);
-    $area=$_POST['area'];
-    $price=$_POST['price'];
-    $type=ucwords($_POST['type']);
-    $valid=$_POST['valid'];
-    $beds=$_POST['beds'];
-    $baths=$_POST['baths'];
-    $description=ucfirst($_POST['description']);
-    $image='sale11.jpg';
-    $status='available';
-    $date=date("Y-m-d");
-    $image=$_FILES['image']['name'];
-    $target2 = "img/house/".basename($image);
-    $sql="INSERT INTO `property` (`id`, `agent_id`, `name`, `address`, `area`, `price`, `type`, `valid_time`, `beds`, `baths`, `description`, `other_facilities`, `image`, `video`, `floor_plan`, `status`, `report`, `date`) 
+    $id = $_SESSION['id'];
+    $name = ucwords($_POST['name']);
+    $address = ucwords($_POST['address']);
+    $area = $_POST['area'];
+    $price = $_POST['price'];
+    $type = ucwords($_POST['type']);
+    $valid = $_POST['valid'];
+    $beds = $_POST['beds'];
+    $baths = $_POST['baths'];
+    $description = ucfirst($_POST['description']);
+    $image = 'sale11.jpg';
+    $status = 'available';
+    $date = date("Y-m-d");
+    $image = $_FILES['image']['name'];
+    $target2 = "img/house/" . basename($image);
+    $sql = "INSERT INTO `property` (`id`, `agent_id`, `name`, `address`, `area`, `price`, `type`, `valid_time`, `beds`, `baths`, `description`, `other_facilities`, `image`, `video`, `floor_plan`, `status`, `report`, `date`) 
                                     VALUES (NULL,'$id', '$name', '$address', '$area', '$price', '$type', '$valid', '$beds', '$baths', '$description', NULL, '$image', NULL, NULL, '$status', NULL, '$date')";
-    mysqli_query($conn,$sql);
-    if  (move_uploaded_file($_FILES['image']['tmp_name'], $target2)) {
-        echo"<script>alert('New Agent Added !');</script>";
+    mysqli_query($conn, $sql);
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $target2)) {
+        echo "<script>alert('New Agent Added !');</script>";
         header("Location:AAindex.php");
+    } else {
+        echo "<script>alert('Something went wrong! Please Retry');</script>";
     }
-    else {
-        echo"<script>alert('Something went wrong! Please Retry');</script>";
-    }
-
 }
 ?>
